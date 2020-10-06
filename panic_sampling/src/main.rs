1bde240ac01cc4469e88d601acf098b87b629817
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2020-10-06 19:18:11 +0200

    cargo fmt

--- panic_sampling/src/main.rs
+++ panic_sampling/src/main.rs
@@ -1,18 +1,20 @@
-use rand::{Rng, RngCore, Error, rngs::ThreadRng, thread_rng};
 use rand::distributions::Distribution;
+use rand::{rngs::ThreadRng, thread_rng, Error, Rng, RngCore};
 use rand_distr::StandardNormal;
 
 struct BoxStdNormD;
 
-impl  Distribution<Box<f64>> for BoxStdNormD {
+impl Distribution<Box<f64>> for BoxStdNormD {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Box<f64> {
         Box::new(StandardNormal.sample(rng))
     }
 }
 
-
-const N : u32 = 20;
-struct PanicRng {counter : u32, rng: ThreadRng}
+const N: u32 = 20;
+struct PanicRng {
+    counter: u32,
+    rng: ThreadRng,
+}
 
 impl RngCore for PanicRng {
     fn next_u32(&mut self) -> u32 {
@@ -45,8 +47,11 @@ impl RngCore for PanicRng {
 }
 
 fn main() {
-    let mut rng = PanicRng { counter : 0, rng: thread_rng() };
+    let mut rng = PanicRng {
+        counter: 0,
+        rng: thread_rng(),
+    };
     let distr = rand_array_iid::IIDDistr::new(BoxStdNormD);
-    let x : [Box<f64>;31] = distr.sample(&mut rng);
-    println!("{:?}",&x)
+    let x: [Box<f64>; 31] = distr.sample(&mut rng);
+    println!("{:?}", &x)
 }
